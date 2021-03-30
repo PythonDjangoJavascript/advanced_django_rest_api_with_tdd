3df8c9bdf25ef5bdd427aa0f6f91b2caae07780f
Sayed <nuruddinsayeed@gmail.com>
2021-03-30 20:39:38 +0600

    user serializer model added

---
+++ app/user/serializers.py
@@ -0,0 +1,25 @@
+from django.contrib.auth import get_user_model, models
+
+from rest_framework import serializers
+
+
+class UserSerializser(serializers.ModelSerializer):
+    """Serializer for the user object"""
+
+    class Meta:
+        """Meta class defins the models and its attributes"""
+
+        model = get_user_model()
+        fields = ("email", "password", "name")
+        extra_kwargs = {
+            "password": {
+                "write_only": True,
+                "min_length": 5
+            }
+        }
+
+        def create(self, validated_data):
+            """we are overriding default create to use our
+            encripted password method"""
+
+            return get_user_model().objects.create_user(**validated_data)
